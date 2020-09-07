76c42ad80537584e0f009f8131828965b1c71931
snj0726 <69622308+snj0726@users.noreply.github.com>
2020-09-07 16:56:01 +1200

    Search Bar Dev
    Enabled google maps zoom buttons
    Search bar now has functionality:
     - finds matching locations
     - moves camera to location
     - adds marker to location
    Keyboard hidden on pressing enter in search bar.

--- app/src/main/java/com/example/bikebuddy/MapsActivity.java
+++ app/src/main/java/com/example/bikebuddy/MapsActivity.java
@@ -2,7 +2,19 @@ package com.example.bikebuddy;
 
 import androidx.fragment.app.FragmentActivity;
 
+import android.content.Context;
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
+import android.renderscript.ScriptGroup;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.WindowManager;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -12,8 +24,13 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
+    private static final String TAG = "";
     private GoogleMap mMap;
     //push test PK
     @Override
@@ -29,6 +46,76 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     }
 
+    // search bar text
+    private EditText mSearchText;
+
+    // initialise search bar
+    private void initSearchBar() {
+        Log.d(TAG, "initSearchBar: initialising");
+        mSearchText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int actionID, KeyEvent keyEvent) {
+                if(actionID == EditorInfo.IME_ACTION_SEARCH
+                    || actionID == EditorInfo.IME_ACTION_DONE
+                    || keyEvent.getAction() == keyEvent.ACTION_DOWN
+                    || keyEvent.getAction() == keyEvent.KEYCODE_ENTER
+                    || keyEvent.getAction() == keyEvent.KEYCODE_NUMPAD_ENTER)
+                {
+                    geoLocateBySearch();
+                }
+
+                return false;
+            }
+        });
+    }
+
+    // perform search
+    private void geoLocateBySearch() {
+        Log.d(TAG, "geoLocateBySearch: starting");
+
+        String searchString = mSearchText.getText().toString();
+
+        Geocoder geocoder = new Geocoder(MapsActivity.this);
+        List<Address> addressList = new ArrayList<>();
+        try {
+            // try adding found results into list (currently only 1 result max)
+            addressList = geocoder.getFromLocationName(searchString, 1);
+
+        }catch (IOException e) {
+            Log.e(TAG, "geoLocateBySearch: Exception caught: " + e.getMessage());
+        }
+
+        hideKeyboard();
+
+        // if address results were added to list:
+        if(addressList.size() > 0) {
+            // grab address
+            Address address = addressList.get(0);
+
+            // output address to log and toast
+            Toast.makeText(this, address.toString(), Toast.LENGTH_LONG).show();
+            Log.d(TAG, "geoLocateBySearch: found address: " + address.toString());
+
+            // store latitude and longitude
+            LatLng foundLocation = new LatLng(address.getLatitude(), address.getLongitude());
+            // go to found location
+            mMap.clear();
+            mMap.animateCamera(CameraUpdateFactory.newLatLng(foundLocation));
+            // make marker
+            MarkerOptions searchedLocationMarker = new MarkerOptions().position(foundLocation).title(address.getAddressLine(0));
+            mMap.addMarker(searchedLocationMarker);
+        }
+    }
+
+    // hide the popup keyboard
+    private void hideKeyboard() {
+        View view = this.getCurrentFocus();
+        if (view != null) {
+            InputMethodManager inputMethodManager = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+            inputMethodManager.hideSoftInputFromWindow(view.getWindowToken(), 0);
+        }
+    }
+
     /**
      * Manipulates the map once available.
      * This callback is triggered when the map is ready to be used.
@@ -41,6 +128,14 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+
+        // stock google maps UI buttons
+        mMap.getUiSettings().setZoomControlsEnabled(true);
+
+        // search bar init stuff
+        mSearchText = (EditText) findViewById(R.id.input_search);
+        initSearchBar();
+
         // Add a marker in Sydney and move the camera
         LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));

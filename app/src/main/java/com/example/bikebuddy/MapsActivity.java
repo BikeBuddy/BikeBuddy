841a6dd076827543c8572d27fd5c1dbe8812cfb3
snj0726 <69622308+snj0726@users.noreply.github.com>
2020-09-13 13:59:39 +1200

    Autocomplete done(?)
    autocomplete search now has the same functionality of the original search.
    It just looks kinda gross and you have to select a location.
    But it's needed for the autocomplete so I guess it's worth it

--- app/src/main/java/com/example/bikebuddy/MapsActivity.java
+++ app/src/main/java/com/example/bikebuddy/MapsActivity.java
@@ -1,26 +1,12 @@
 package com.example.bikebuddy;
 
 import androidx.annotation.NonNull;
-import androidx.core.view.MotionEventCompat;
 import androidx.fragment.app.FragmentActivity;
 
 import android.content.Context;
-import android.location.Address;
-import android.location.Geocoder;
 import android.os.Bundle;
-import android.renderscript.ScriptGroup;
-import android.util.Log;
-import android.view.KeyEvent;
-import android.view.MotionEvent;
 import android.view.View;
-import android.view.WindowManager;
-import android.view.inputmethod.EditorInfo;
 import android.view.inputmethod.InputMethodManager;
-import android.widget.AutoCompleteTextView;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.RelativeLayout;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.common.api.Status;
@@ -31,18 +17,12 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.libraries.places.api.Places;
-import com.google.android.libraries.places.api.model.AutocompletePrediction;
 import com.google.android.libraries.places.api.model.Place;
-import com.google.android.libraries.places.api.model.TypeFilter;
-import com.google.android.libraries.places.api.net.FindAutocompletePredictionsRequest;
 import com.google.android.libraries.places.api.net.PlacesClient;
 import com.google.android.libraries.places.widget.AutocompleteSupportFragment;
 import com.google.android.libraries.places.widget.listener.PlaceSelectionListener;
 
-import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
@@ -61,9 +41,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     }
 
-    // search bar text
-    private EditText mSearchText;
-
+    // init data for autocomplete to store
+    private LatLng autoCompleteLatLng;
+    // initialise places API
     private void initPlaces() {
         // Initialize Places.
         Places.initialize(getApplicationContext(), getResources().getString(R.string.google_maps_key));
@@ -71,19 +51,34 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         // Create a new Places client instance.
         PlacesClient placesClient = Places.createClient(this);
     }
-
+    // initialise autocomplete search bar
     private void initAutoComplete() {
         final AutocompleteSupportFragment autocompleteSupportFragment =
                 (AutocompleteSupportFragment) getSupportFragmentManager().findFragmentById(R.id.autocomplete_fragment);
 
-        autocompleteSupportFragment.setPlaceFields(Arrays.asList(Place.Field.ID, Place.Field.LAT_LNG, Place.Field.NAME));
+        // restrict place field results to ID, Address, LatLng, and Name (basic data, no extra fees)
+        autocompleteSupportFragment.setPlaceFields(Arrays.asList(Place.Field.ID, Place.Field.ADDRESS, Place.Field.LAT_LNG, Place.Field.NAME));
+
+        // restrict results to nz --- could be changed to grab the user's geolocated country.
+        autocompleteSupportFragment.setCountry("nz");
 
         autocompleteSupportFragment.setOnPlaceSelectedListener((new PlaceSelectionListener() {
             @Override
             public void onPlaceSelected(@NonNull Place place) {
-                final LatLng latLng = place.getLatLng();
+                // grab found location data from 'place'
+                // currently just grabbing LatLng for marker making
+                autoCompleteLatLng = place.getLatLng();
 
-                Toast.makeText(MapsActivity.this, "LAT"+latLng.latitude+"\nLONG"+latLng.longitude, Toast.LENGTH_LONG).show();
+                // display found lat long (for debugging)
+                //Toast.makeText(MapsActivity.this, "LAT"+autoCompleteLatLng.latitude+"\nLONG"+autoCompleteLatLng.longitude, Toast.LENGTH_LONG).show();
+
+                // remove existing markers (get rid of this in final so markers added by other things aren't removed)
+                mMap.clear();
+                // go to found location
+                mMap.animateCamera(CameraUpdateFactory.newLatLng(autoCompleteLatLng));
+                // make marker
+                MarkerOptions searchedLocationMarker = new MarkerOptions().position(autoCompleteLatLng).title(place.getAddress());
+                mMap.addMarker(searchedLocationMarker);
             }
 
             @Override
@@ -96,85 +91,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
 
 
- /*   // initialise search bar
-    private void initSearchBar() {
-        Log.d(TAG, "initSearchBar: initialising");
-        mSearchText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
-            @Override
-            public boolean onEditorAction(TextView textView, int actionID, KeyEvent keyEvent) {
-                if(actionID == EditorInfo.IME_ACTION_SEARCH
-                        || actionID == EditorInfo.IME_ACTION_DONE
-                        || keyEvent.getAction() == keyEvent.ACTION_DOWN
-                        || keyEvent.getAction() == keyEvent.KEYCODE_ENTER
-                        || keyEvent.getAction() == keyEvent.KEYCODE_NUMPAD_ENTER)
-                {
-                    geoLocateBySearch();
-                }
-                return false;
-            }
-        });
-    }
-*/
- /*   // perform search
-    private void geoLocateBySearch() {
-        Log.d(TAG, "geoLocateBySearch: starting");
-
-        String searchString = mSearchText.getText().toString();
-
-        Geocoder geocoder = new Geocoder(MapsActivity.this);
-        List<Address> addressList = new ArrayList<>();
-        try {
-            // try adding found results into list (currently only 1 result max)
-            addressList = geocoder.getFromLocationName(searchString, 1);
-
-        }catch (IOException e) {
-            Log.e(TAG, "geoLocateBySearch: Exception caught: " + e.getMessage());
-        }
-
-        hideKeyboard();
-
-        // if address results were added to list:
-        if(addressList.size() > 0) {
-            // grab address
-            Address address = addressList.get(0);
-            // output address to log
-            Log.d(TAG, "geoLocateBySearch: found address: " + address.toString());
-            // store latitude and longitude
-            LatLng foundLocation = new LatLng(address.getLatitude(), address.getLongitude());
-            // go to found location
-            mMap.clear();
-            mMap.animateCamera(CameraUpdateFactory.newLatLng(foundLocation));
-            // make marker
-            MarkerOptions searchedLocationMarker = new MarkerOptions().position(foundLocation).title(address.getAddressLine(0));
-            mMap.addMarker(searchedLocationMarker);
-        }
-    }
-*/
-    // hide the popup keyboard
-    private void hideKeyboard() {
-        View view = this.getCurrentFocus();
-        if (view != null) {
-            InputMethodManager inputMethodManager = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
-            inputMethodManager.hideSoftInputFromWindow(view.getWindowToken(), 0);
-        }
-    }
-
-    @Override
-    public void onUserInteraction() {
-        hideKeyboard();
-        super.onUserInteraction();
-    }
-
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
@@ -182,14 +98,12 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         // stock google maps UI buttons
         mMap.getUiSettings().setZoomControlsEnabled(true);
 
-        // search bar init stuff
-   //     mSearchText = (EditText) findViewById(R.id.input_search);
-   //     initSearchBar();
+        // call initialisations
+        initPlaces();
         initAutoComplete();
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        // start the camera above nz
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(new LatLng(-42, 172)));
+        mMap.moveCamera(CameraUpdateFactory.zoomTo(5));
     }
 }

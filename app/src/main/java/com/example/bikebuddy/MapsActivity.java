70e2b1a68e65b68c1d384fd5f5c452f12c9b21ca
garyng13 <wrc2979@autuni.ac.nz>
2020-09-13 13:51:24 +1200

    integrate pull weather data function to mapActivity

--- app/src/main/java/com/example/bikebuddy/MapsActivity.java
+++ app/src/main/java/com/example/bikebuddy/MapsActivity.java
@@ -5,6 +5,7 @@ import androidx.fragment.app.FragmentActivity;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
@@ -22,6 +23,14 @@ import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback{
@@ -32,6 +41,11 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private Marker marker1;
     private ArrayList<City> city = new ArrayList<City>();
     private ArrayList<Marker> markerArray = new ArrayList<Marker>();
+    private static String sky = "Raining";
+    private static String cityName = "Hong Kong";
+    private static double lon =0;
+    private static double lat =0;
+
 
     //push test PK
     @Override
@@ -59,20 +73,19 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                     for(Marker m : markerArray){
                         m.setVisible((showLogo));
                     }
-//                    marker.setVisible(showLogo);
-//                    marker1.setVisible(showLogo);
-//                    System.out.println(city.get(0).getName());
                 }else{
                     showLogo = false;
                     for(Marker m : markerArray){
                         m.setVisible((showLogo));
                     }
-//                    marker.setVisible(showLogo);
-//                    marker1.setVisible(showLogo);
                 }
             }
-
         });
+
+        getJSON("http://api.openweathermap.org/data/2.5/weather?q=rome,italy&APPID=d2222fc373d644fa109aea09a4046a3c");
+        getJSON("http://api.openweathermap.org/data/2.5/weather?q=auckland,newzealand&APPID=d2222fc373d644fa109aea09a4046a3c");
+        System.out.println("mainActivity sky");
+        System.out.println(sky);
     }
 
     /**
@@ -99,9 +112,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         // Add a marker in Sydney and move the camera
         for(City c : city){
             LatLng cityLatLng = new LatLng(c.getLat(),c.getLng());
-            System.out.println("debug");
-            System.out.println(c.getLat());
-            System.out.println(c.getLng());
             marker = mMap.addMarker(new MarkerOptions().position(cityLatLng).title("Marker in Halminton").snippet("Population: 300,000")
                     .icon(BitmapDescriptorFactory.fromBitmap(smallMarker)));
             mMap.moveCamera(CameraUpdateFactory.newLatLng(cityLatLng));
@@ -114,23 +124,116 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 }
             });
         }
-//        LatLng hamilton = new LatLng(-37.78333 ,175.28333);
-//        marker = mMap.addMarker(new MarkerOptions().position(hamilton).title("Marker in Auckland").snippet("Population: 1,500,000")
-//                .icon(BitmapDescriptorFactory.fromBitmap(smallMarker)));
-//                marker.setVisible(showLogo);
-//        mMap.moveCamera(CameraUpdateFactory.newLatLng(hamilton));
+    }
 
-//        LatLng auckland = new LatLng(-36.848461, 174.763336);
-//        marker1 = mMap.addMarker(new MarkerOptions().position(auckland).title("Marker in Hamilton").snippet("Population: 400,000")
-//                .icon(BitmapDescriptorFactory.fromBitmap(smallMarker)));
-//                marker.setVisible(showLogo);
-//        mMap.moveCamera(CameraUpdateFactory.newLatLng(auckland));
-//        mMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
-//            @Override
-//            public boolean onMarkerClick(Marker marker) {
-//                System.out.println("marker clicked");
-//                return false;
-//            }
-//        });
+    //this method is actually fetching the json string
+    private void getJSON(final String urlWebService) {
+        /*
+         * As fetching the json string is a network operation
+         * And we cannot perform a network operation in main thread
+         * so we need an AsyncTask
+         * The constrains defined here are
+         * Void -> We are not passing anything
+         * Void -> Nothing at progress update as well
+         * String -> After completion it should return a string and it will be the json string
+         * */
+        class GetJSON extends AsyncTask<Void, Void, String> {
+            //this method will be called before execution
+            //you can display a progress bar or something
+            //so that user can understand that he should wait
+            //as network operation may take some time
+            @Override
+            protected void onPreExecute() {
+                super.onPreExecute();
+                Toast.makeText(getApplicationContext(), "onPreExecute is working", Toast.LENGTH_LONG).show();
+            }
+
+            //this method will be called after execution
+            //so here we are displaying a toast with the json string
+            @Override
+            protected void onPostExecute(String s) {
+                super.onPostExecute(s);
+                Toast.makeText(getApplicationContext(), s, Toast.LENGTH_LONG).show();
+                try {
+                    JSONObject obj = new JSONObject(s);
+                    System.out.println("debug");
+                    System.out.println(obj);
+                    JSONArray weather = obj.getJSONArray("weather");
+                    JSONObject coord = obj.getJSONObject("coord");
+                    System.out.println(coord);
+                    System.out.println("Rome or Auckland?");
+                    String cityName = obj.getString("name");
+                    System.out.println(cityName);
+                    JSONObject weatherArray0 = weather.getJSONObject(0);
+                    String lon = coord.getString("lon");
+                    String lat = coord.getString("lat");
+                    String sky = weatherArray0.getString("description");
+                    String icon = weatherArray0.getString("icon");
+                    System.out.println(obj);
+                    System.out.println(weather);
+                    System.out.println(weatherArray0);
+                    System.out.println("sky");
+                    System.out.println(sky);
+                    System.out.println("icon");
+                    System.out.println(icon);
+                    System.out.println("lon");
+                    System.out.println(lon);
+                    System.out.println("lat");
+                    System.out.println(lat);
+                    MapsActivity.sky = sky;
+                    MapsActivity.cityName = cityName;
+                    MapsActivity.lon = Double.parseDouble(lon);
+                    MapsActivity.lat = Double.parseDouble(lat);
+                    System.out.println("MapsActivity sky: ");
+                    System.out.println(MapsActivity.sky);
+                    System.out.println("MapsActivity cityName: ");
+                    System.out.println(MapsActivity.cityName);
+                    System.out.println("MapsActivity lon: ");
+                    System.out.println(MapsActivity.lon);
+                    System.out.println("MapsActivity lat: ");
+                    System.out.println(MapsActivity.lat);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            //in this method we are fetching the json string
+            @Override
+            protected String doInBackground(Void... voids) {
+
+                try {
+                    //creating a URL
+                    URL url = new URL(urlWebService);
+
+                    //Opening the URL using HttpURLConnection
+                    HttpURLConnection con = (HttpURLConnection) url.openConnection();
+
+                    //StringBuilder object to read the string from the service
+                    StringBuilder sb = new StringBuilder();
+
+                    //We will use a buffered reader to read the string from service
+                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(con.getInputStream()));
+
+                    //A simple string to read values from each line
+                    String json;
+
+                    //reading until we don't find null
+                    while ((json = bufferedReader.readLine()) != null) {
+
+                        //appending it to string builder
+                        sb.append(json + "\n" + "gary");
+                    }
+                    System.out.println("printing sb");
+                    System.out.println(sb);
+                    //finally returning the read string
+                    return sb.toString().trim();
+                } catch (Exception e) {
+                    return null;
+                }
+            }
+        }
+        //creating asynctask object and executing it
+        GetJSON getJSON = new GetJSON();
+        getJSON.execute();
     }
 }

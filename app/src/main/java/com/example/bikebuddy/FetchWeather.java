3b788ce923a76769a114df319a23f8559080ab2d
s-ward <63839495+s-ward@users.noreply.github.com>
2020-09-21 15:17:50 +1200

    Update FetchWeather.java

--- app/src/main/java/com/example/bikebuddy/FetchWeather.java
+++ app/src/main/java/com/example/bikebuddy/FetchWeather.java
@@ -13,16 +13,13 @@ public class FetchWeather
     }
 
     //option 1: pass in MapsActivity
-    public String fetch(double lat, double lon) {
+    public void fetch(double lat, double lon) {
 
         String st = "https://api.openweathermap.org/data/2.5/weather?lat="+lat+"&lon="+lon+"&APPID=d2222fc373d644fa109aea09a4046a3c";
 
        // gj.execute(st); //error trying to run multiple executes, so have to create new getJSON each time
         new GetJSON(ma).execute(st);
 
-        String s = "description";
-
-        return s;
     }
 
     //option 2: extend MapsActivity to access it's methods

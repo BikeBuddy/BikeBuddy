2ba38b74e7afd64f58751e0cd6f285ccdc38e034
peterkim1994 <52155345+peterkim1994@users.noreply.github.com>
2020-09-25 23:56:49 +1200

    .

--- app/src/test/java/com/example/bikebuddy/TripTest.java
+++ app/src/test/java/com/example/bikebuddy/TripTest.java
@@ -13,7 +13,7 @@ public class TripTest {
         Trip testTrip = new Trip();
         for(int i = 1; i<6000; i++ ){
             testTrip.duration = i;
-            Assert.assertEquals( ((float)(i/600)), testTrip.getDuration(), 0.01f);
+            Assert.assertEquals( ((float)(i/60)), testTrip.getDuration(), 0.01f);
         }
     }
 

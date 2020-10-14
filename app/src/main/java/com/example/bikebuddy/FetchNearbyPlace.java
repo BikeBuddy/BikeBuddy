fec2bdd526f213076dd417a4e2c56260b82dc504
garyng13 <wrc2979@autuni.ac.nz>
2020-10-14 21:54:38 +1300

    restaurant

--- app/src/main/java/com/example/bikebuddy/FetchNearbyPlace.java
+++ app/src/main/java/com/example/bikebuddy/FetchNearbyPlace.java
@@ -15,7 +15,7 @@ public class FetchNearbyPlace {
 
         //String st = "https://api.openweathermap.org/data/2.5/weather?lat="+lat+"&lon="+lon+"&APPID=d2222fc373d644fa109aea09a4046a3c";
         //String st = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=1500&type=restaurant&keyword=cruise&key=AIzaSyBKjjjLBTAk_0vWHH2S4yzgDvQKDWGu2-w";
-        String st = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location="+lat+","+lon+"&radius=1500&type=gas&key=AIzaSyBKjjjLBTAk_0vWHH2S4yzgDvQKDWGu2-w";
+        String st = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location="+lat+","+lon+"&radius=1500&type=restaurant&keyword=cruise&key=AIzaSyBKjjjLBTAk_0vWHH2S4yzgDvQKDWGu2-w";
 
         // gj.execute(st); //error trying to run multiple executes, so have to create new getJSON each time
         new GetPlaceJSON(mapsActivity).execute(st);

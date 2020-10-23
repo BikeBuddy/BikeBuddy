6aa5ef440c72f7b4252621cb49254ba06b218e67
garyng13 <wrc2979@autuni.ac.nz>
2020-10-23 21:09:51 +1300

    add unit test

---
+++ app/src/androidTest/java/com/example/bikebuddy/PlaceFunctionTest.java
@@ -0,0 +1,13 @@
+package com.example.bikebuddy;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PlaceFunctionTest {
+
+    @Test
+    public void addLocationsPlace() {
+        
+    }
+}

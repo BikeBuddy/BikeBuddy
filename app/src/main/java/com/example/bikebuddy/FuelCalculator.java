004a9798417ec3a2d34c544d943ac25a4307db26
peterkim1994 <52155345+peterkim1994@users.noreply.github.com>
2020-10-27 16:35:04 +1300

    Revert "Merge branch 'masterPK' of https://github.com/BikeBuddy/BikeBuddy into masterPK"
    This reverts commit 8435381fc2f102da6833ea15e495355abf56843d, reversing
    changes made to 85f22e7e5850dd3dd051b80c700b54687c633772.

---
+++ app/src/main/java/com/example/bikebuddy/FuelCalculator.java
@@ -0,0 +1,34 @@
+package com.example.bikebuddy;
+
+import android.widget.TextView;
+
+/*
+ * Author Theo Brown
+ *
+ * Deprecated class until vehicle data is saved and can be accessed properly. Use TripFuelCalculator.java in the meantime.
+ */
+@Deprecated
+public class FuelCalculator {
+    /*
+     * Returns the Km/L of the completed trip
+     */
+    public double calculateTripKmL(Trip trip, Vehicle v) {
+        return (trip.distance / v.getFuelUsed());
+    }
+    /*
+     * Returns the predicted maximum range of a vehicle, assuming a full tank and consistent fuel usage.
+     */
+    public double calculateMaxRange(Vehicle v) {
+        return (v.getFuelTankSize() / v.getAvgKmPerLitre());
+    }
+    /*
+     *Returns false if trip is longer than the max range of the vehicle, returns true otherwise
+     */
+    public boolean calculateTripSufficientFuel(Trip trip, Vehicle v) {
+        //FIXME
+        if (trip.distance > calculateMaxRange(v))
+            return false;
+        else
+            return true;
+    }
+}
